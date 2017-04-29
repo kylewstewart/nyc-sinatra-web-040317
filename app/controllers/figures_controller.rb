ee770d637aa301d36e3e93b3c784c04c61d16af4
kylewstewart <kyle.w.stewart@mac.com>
2017-04-29 13:30:41 -0400

    still working

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -15,23 +15,32 @@ class FiguresController < ApplicationController
 
 
   post '/figures' do
-    params[:figure][:landmark_ids] << Landmark.create(params[:landmark]).id if params[:landmark][:name] != ""
-    params[:figure][:title_ids] << Title.create(params[:title]).id if params[:title][:name] != ""
-
-    figure = Figure.create(params[:figure])
+    params[:figure][:landmark_ids] << Landmark.create(params[:landmark]).id unless params[:landmark][:name] == ""
+    params[:figure][:title_ids] << Title.create(params[:title]).id unless params[:title][:name] == ""
+    figure = Figure.create
+    figure.update(params[:figure])
     redirect to ('/figures')
   end
 
   get '/figures/:id' do
     @figure = Figure.find(params[:id])
+    @landmarks = Landmark.where(figure_id: params[:id])
+    figure_titles = FigureTitle.where(figure_id: params[:id])
+    @titles = figure_titles.collect {|figure_title| Title.find(figure_title.title_id)}
     erb :'/figures/show.html'
   end
 
   get '/figures/:id/edit' do
     @figure = Figure.find(params[:id])
-    erb :'/figures/show.html'
+    @landmarks = Landmark.order(:name)
+    @figure_titles = FigureTitle.where(figure_id: params[:id])
+    @titles = Title.order(:name)
+    erb :'/figures/edit.html'
   end
 
-
+  delete '/figures/:id' do      #DESTROY
+    Figure.find(params[:id]).destroy
+    redirect to ('/figures')
+  end
 
 end
